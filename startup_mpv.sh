c5cf887a5f23fd2ae722380af71c694db7c8e280
原田勇大 <haradayuudai@ExhibitMacmini3.local>
2023-03-18 02:16:06 +0900

    fix

--- startup_mpv.sh
+++ startup_mpv.sh
@@ -4,6 +4,5 @@ nrow=`pgrep 'mpv -' | wc -l`
 if [ $nrow -ge 1 ]; then
     echo 'alread active'
 else 
-    mpv - -fs --input-ipc-server=/tmp/mpvsocket --keep-open --keep-open-pause=no -vo=gpu
-
+    mpv - -fs --input-ipc-server=/tmp/mpvsocket --keep-open --keep-open-pause=no -vo=gpu -hwdec=no
 fi
